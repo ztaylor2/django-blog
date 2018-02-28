7d8a8aa57c8f116a9f2f8b4035e16e033bed3f36
ztaylor2 <ztaylor2@zagmail.gonzaga.edu>
2018-02-28 15:59:21 -0800

    rendering hello world with class based view.

--- blog/blog/urls.py
+++ blog/blog/urls.py
@@ -1,4 +1,4 @@
-"""blog URL Configuration
+"""blog URL Configuration.
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/2.0/topics/http/urls/
@@ -16,8 +16,12 @@ Including another URLconf
 
 from django.urls import include, path
 from django.contrib import admin
+from blog.views import HomeView
+from django.conf.urls import url
+
 
 urlpatterns = [
     path('posts/', include('posts.urls')),
     path('admin/', admin.site.urls),
+    url(r'^$', HomeView.as_view(), name='home'),
 ]
