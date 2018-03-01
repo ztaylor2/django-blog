f7cc543fbea9dc9cfae9e694d23538e5d68c15ea
ztaylor2 <ztaylor2@zagmail.gonzaga.edu>
2018-02-28 19:44:08 -0800

    built the database model for a blog post.

--- blog/posts/models.py
+++ blog/posts/models.py
@@ -1,3 +1,10 @@
+"""Database models for posts."""
 from django.db import models
 
-# Create your models here.
+
+class Post(models.Model):
+    """The database model for a blog post."""
+
+    title = models.CharField(max_length=50)
+    body = models.TextField()
+    publication_date = models.DateField(auto_now_add=True)
